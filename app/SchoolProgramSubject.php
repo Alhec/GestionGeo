6b7db9a6fca3826997750bd1701644e494b87246
alhec <hector080896@gmail.com>
2021-02-03 16:34:51 -0400

    comments models

--- app/SchoolProgramSubject.php
+++ app/SchoolProgramSubject.php
@@ -5,14 +5,37 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 
+/**
+ * @package : Model
+ * @author : Hector Alayon
+ * @version : 1.0
+ */
 class SchoolProgramSubject extends Model
 {
-    protected $table = 'school_program_subject';
-
-    protected $fillable = ['school_program_id','subject_id','type','subject_group'];
-
+    /**
+     * Omite los campos de fecha de creado y modificado en las tablas
+     *
+     */
     public $timestamps = false;
 
+    /**
+     * Los atributos que se pueden asignar en masa.
+     *
+     * @var array
+     */
+    protected $fillable = ['school_program_id','subject_id','type','subject_group'];
+
+    /**
+     * Nombre de la tabla asociada
+     *
+     */
+    protected $table = 'school_program_subject';
+
+    /**
+     *Crea una asociacion de materia con programa escolar en el sistema
+     * @param mixed $schoolProgramSubject: Objeto de tipo schoolProgramSubject (contiene los atributos del modelo)
+     * @return integer Crea un objeto de SchoolProgramSubject, si falla devolverá 0.
+     */
     public static function addSchoolProgramSubject($schoolProgramSubject)
     {
         try{
@@ -23,6 +46,12 @@ class SchoolProgramSubject extends Model
         }
     }
 
+    /**
+     *Obtiene la asociacion de una materia con un programa escolar
+     * @param string $subjectId: Id de la materia
+     * @param string $schoolProgramId: Id del programa escolar
+     * @return SchoolProgramSubject|integer Obtiene el objeto SchoolProgramSubject dado un subjectId y schoolProgramId.
+     */
     public static function getSchoolProgramSubjectBySubjectAndSchoolProgram($subjectId,$schoolProgramId)
     {
         try{
@@ -34,6 +63,11 @@ class SchoolProgramSubject extends Model
         }
     }
 
+    /**
+     *Obtiene la asociacion de una materia con todos sus programas escolares asociados
+     * @param string $subjectId: Id de la materia
+     * @return SchoolProgramSubject|integer Obtiene los programas escolares a los cuales está asociada la materia.
+     */
     public static function getSchoolProgramSubjectsBySubjectId($subjectId)
     {
         try{
@@ -44,6 +78,12 @@ class SchoolProgramSubject extends Model
         }
     }
 
+    /**
+     *Actualiza una asociacion de materia con programa escolar en el sistema
+     * @param integer $id Id del objeto SchoolProgramSubject
+     * @param mixed $schoolProgramSubject: Objeto de tipo schoolProgramSubject (contiene los atributos del modelo)
+     * @return integer Actualiza un objeto schoolProgramSubject dado su id.
+     */
     public static function updateSchoolProgramSubject($id, $schoolProgramSubject)
     {
         self::find($id)
@@ -57,6 +97,11 @@ class SchoolProgramSubject extends Model
         }
     }
 
+    /**
+     *Elimina una asociacion entre materia y programa escolar
+     * @param integer $id Id del objeto SchoolProgramSubject
+     * @return integer Elimina una entidad de tipo SchoolProgramSubject.
+     */
     public static function deleteSchoolProgramSubject($id)
     {
         try{
@@ -68,6 +113,11 @@ class SchoolProgramSubject extends Model
         }
     }
 
+    /**
+     *Obtiene las materia sasociadas aun grupo
+     * @param integer $subjectGroup: Id del grupo.
+     * @return integer Obtiene el conjunto de materias asociadas en caso de que esta esté asociada con otras.
+     */
     public static function getSubjectGroup($subjectGroup)
     {
         try{
