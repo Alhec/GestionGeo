55d43f10654f01e81d74b13ccb46dfdfce1e981c
alhec <hector080896@gmail.com>
2021-03-26 16:28:35 -0400

    controller SchoolPeriod doc

--- app/Http/Controllers/SchoolPeriodController.php
+++ app/Http/Controllers/SchoolPeriodController.php
@@ -5,12 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Services\SchoolPeriodService;
 
+/**
+ * @package : Controller
+ * @author : Hector Alayon
+ * @version : 1.0
+ */
 class SchoolPeriodController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @param Request $request
+     * Obtiene todos los periodos escolares de una organización usa el método
+     * SchoolPeriodService::getSchoolPeriods($organizationId) o
+     * SchoolPeriodService::getSchoolPeriods($organizationId,$perPage) si usa paginación.
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function index(Request $request)
@@ -19,12 +25,10 @@ class SchoolPeriodController extends Controller
         $perPage = $request->input('per_page');
         return $perPage ? SchoolPeriodService::getSchoolPeriods($organizationId,$perPage) :
             SchoolPeriodService::getSchoolPeriods($organizationId);
-
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
+     * Agrega un periodo escolar, usa el método SchoolPeriodService::addSchoolPeriod($request,$organizationId).
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
@@ -35,10 +39,10 @@ class SchoolPeriodController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int $id
-     * @param Request $request
+     * Devuelve los datos de un periodo escolar dado su id, usa el método
+     * SchoolPeriodService::getSchoolPeriodById($id,$organizationId).
+     * @param  int  $id
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function show($id,Request $request)
@@ -48,10 +52,10 @@ class SchoolPeriodController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
+     * Actualiza los datos de un periodo escolar usando el método
+     * SchoolPeriodService::updateSchoolPeriod($request,$id,$organizationId).
      * @param  int  $id
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -61,10 +65,10 @@ class SchoolPeriodController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
-     *
-     * @param Request $request
-     * @param  int $id
+     * Elimina un periodo escolar dado su id usando el método
+     * SchoolPeriodService::deleteSchoolPeriod($id,$organizationId).
+     * @param  int  $id
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function destroy(Request $request, $id)
@@ -73,12 +77,23 @@ class SchoolPeriodController extends Controller
         return SchoolPeriodService::deleteSchoolPeriod($id,$organizationId);
     }
 
+    /**
+     * Devuelve el periodo escolar actual usando el método SchoolPeriodService::getCurrentSchoolPeriod($organizationId).
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function current(Request $request)
     {
         $organizationId = $request->header('Organization-Key');
         return SchoolPeriodService::getCurrentSchoolPeriod($organizationId);
     }
 
+    /**
+     * Obtiene las asignaturas que dicta un profesor usando el método
+     * SchoolPeriodService::getSubjectsTaughtSchoolPeriod($teacherId,$organizationId).
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function subjectTaughtSchoolPeriod(Request $request)
     {
         $organizationId = $request->header('Organization-Key');
