e148e5b4ffc831f020d18ba09e3ac1959ad2d490
alhec <hector080896@gmail.com>
2021-03-26 18:17:32 -0400

    controller annual report doc

--- app/Http/Controllers/AnnualReportController.php
+++ app/Http/Controllers/AnnualReportController.php
@@ -11,8 +11,19 @@ use App\Services\AnnualReportService;
 use Excel;
 use Illuminate\Http\Request;
 
+/**
+ * @package : Controller
+ * @author : Hector Alayon
+ * @version : 1.0
+ */
 class AnnualReportController extends Controller
 {
+    /**
+     * Invoca el servicio que genera el reporte anual con el metodo
+     * AnnualReportService::exportAnnualReport($firstSchoolPeriodId,$lastSchoolPeriodId,$organizationId)
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|Excel|object
+     */
     public static function exportAnnualReport(Request $request)
     {
         $organizationId = $request->header('Organization-Key');
