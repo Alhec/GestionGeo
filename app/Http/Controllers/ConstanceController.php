76b392fe7601cb78bcb8825e4101564ecf5e60ae
alhec <hector080896@gmail.com>
2021-03-26 18:06:43 -0400

    controller constance doc

--- app/Http/Controllers/ConstanceController.php
+++ app/Http/Controllers/ConstanceController.php
@@ -6,8 +6,19 @@ use App\Services\ConstanceService;
 use Illuminate\Http\Request;
 use phpDocumentor\Reflection\Types\Integer;
 
+/**
+ * @package : Controller
+ * @author : Hector Alayon
+ * @version : 1.0
+ */
 class ConstanceController extends Controller
 {
+    /**
+     * Genera una constancia de estudios o devuelve la data necesaria para generar una con el método
+     * ConstanceService::constanceOfStudy($request,$studentId,$organizationId,$data).
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response|array|\PDF|object
+     */
     public function constanceOfStudy(Request $request)
     {
         $studentId = $request->input('student_id');
@@ -16,6 +27,12 @@ class ConstanceController extends Controller
         return ConstanceService::constanceOfStudy($studentId,$organizationId,$data);
     }
 
+    /**
+     * Genera una constancia de inscripción de un estudiante o devuelve la data necesaria para generarla usando el
+     * método ConstanceService::inscriptionConstance($request,$studentId,$inscriptionId,$organizationId,$data).
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response|array|\PDF|object
+     */
     public function inscriptionConstance(Request $request)
     {
         $studentId = $request->input('student_id');
@@ -25,6 +42,12 @@ class ConstanceController extends Controller
         return ConstanceService::inscriptionConstance($studentId,$inscriptionId,$organizationId,$data);
     }
 
+    /**
+     * Genera una constancia de trabajo o devuelve la data necesaria para generar una con el método
+     * ConstanceService::constanceOfWorkTeacher($teacherId,$organizationId,$data).
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response|array|\PDF|object
+     */
     public function constanceOfWorkTeacher(Request $request)
     {
         $teacherId = $request->input('teacher_id');
@@ -33,6 +56,12 @@ class ConstanceController extends Controller
         return ConstanceService::constanceOfWorkTeacher($teacherId,$organizationId,$data);
     }
 
+    /**
+     * Genera una constancia de trabajo o devuelve la data necesaria para generar una con el método
+     * ConstanceService::constanceOfWorkAdministrator($administratorId,$organizationId,$data)
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response|array|\PDF|object
+     */
     public function constanceOfWorkAdministrator(Request $request)
     {
         $administratorId = $request->input('administrator_id');
@@ -41,6 +70,12 @@ class ConstanceController extends Controller
         return ConstanceService::constanceOfWorkAdministrator($administratorId,$organizationId,$data);
     }
 
+    /**
+     * Genera una constancia de carga académica o devuelve la data necesaria para generar una con el método
+     * ConstanceService::academicLoad($studentId,$organizationId,$data).
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response|array|\PDF|object
+     */
     public function academicLoad(Request $request)
     {
         $studentId = $request->input('student_id');
@@ -49,6 +84,12 @@ class ConstanceController extends Controller
         return ConstanceService::academicLoad($studentId,$organizationId,$data);
     }
 
+    /**
+     * Genera una constancia de histórica o devuelve la data necesaria para generar una con el método
+     * ConstanceService::studentHistorical($studentId,$organizationId,$data).
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response|array|\PDF|object
+     */
     public function studentHistorical(Request $request)
     {
         $studentId = $request->input('student_id');
