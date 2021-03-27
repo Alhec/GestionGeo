cdd47d2d67fe716efa266e71ed86f17c01972bc0
alhec <hector080896@gmail.com>
2021-03-27 13:23:58 -0400

    middleware app

---
+++ app/Http/Middleware/AppAuthorization.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+/**
+ * @package : Middleware
+ * @author : Hector Alayon
+ * @version : 1.0
+ */
+class AppAuthorization
+{
+    /**
+     * Valida que la app web use la llave de autorizacion para hacer uso del Api.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        $appAuth = $request->header('App-Auth');
+        if ($appAuth === env('APP_NAME', 'GAAPFC')){
+            return response()->json(['error' => 'Unauthorized'],401);
+        }
+        return $next($request);
+    }
+}
